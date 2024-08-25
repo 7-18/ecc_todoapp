fd81e86568c92df8a880796656f982099a84f6e9
Kevin Brian Briceño <kevinbrianbr@gmail.com>
2024-08-25 16:30:46 -0500

    #5 - Add User Schema

---
+++ api/src/models/user.js
@@ -0,0 +1,26 @@
+import { DataTypes } from "sequelize";
+import { db } from "../config/db.js";
+
+export const User = db.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+});
