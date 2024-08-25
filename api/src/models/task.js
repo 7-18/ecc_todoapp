bc35f16d88fdfcea2dfa6f7c0921df1be4b9eb03
Kevin Brian Briceño <kevinbrianbr@gmail.com>
2024-08-25 16:31:02 -0500

    #6 - Add Task Model Schema

---
+++ api/src/models/task.js
@@ -0,0 +1,35 @@
+import { DataTypes } from "sequelize";
+import { db } from "../config/db.js";
+
+export const Task = db.define("Task", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    foreignKey: true,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+});
