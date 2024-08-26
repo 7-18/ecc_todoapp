3d473086e5ce1f696f0cde941d69b506cedbc7f7
Kevin Brian Briceño <kevinbrianbr@gmail.com>
2024-08-25 21:14:18 -0500

    #9 - Task Controller and New field in Schema

---
+++ api/src/controllers/task.js
@@ -0,0 +1,114 @@
+import { Op } from "sequelize";
+import { Task } from "../models/task.js";
+
+export const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ message: "Task created successfully", data: task });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const getTasks = async (req, res) => {
+  const { skip, limit, search, sort } = req.query;
+  try {
+    const tasks = await Task.findAndCountAll({
+      where: search
+        ? {
+            [Op.or]: [
+              { title: { [Op.iLike]: `%${search}%` } },
+              { description: { [Op.iLike]: `%${search}%` } },
+            ],
+          }
+        : null,
+      where: { status: true },
+      order: sort ? [sort.split(":")] : [["priority", "ASC"]],
+      offset: skip ? parseInt(skip) : 0,
+      limit: limit ? parseInt(limit) : 10,
+    });
+    res
+      .status(200)
+      .json({ message: "List of tasks", data: tasks.rows, total: tasks.count });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const getTaskById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findByPk(id);
+    if (task) {
+      res.status(200).json({ message: "Task found", data: task });
+    } else {
+      res.status(404).json({ error: "Task not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [updated] = await Task.update(req.body, {
+      where: { id: id },
+    });
+    if (updated) {
+      const updatedTask = await Task.findByPk(id);
+      res
+        .status(200)
+        .json({ message: "Task updated successfully", data: updatedTask });
+    } else {
+      res.status(404).json({ error: "Task not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const updateTaskStatus = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [updated] = await Task.update(req.body, {
+      where: { id: id },
+    });
+    if (updated) {
+      const updatedTask = await Task.findByPk(id);
+      res.status(200).json({
+        message: "Task status updated successfully",
+        data: updatedTask,
+      });
+    } else {
+      res.status(404).json({ error: "Task not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Task.destroy({
+      where: { id: id },
+    });
+    if (deleted) {
+      res.status(200).json({ message: "Task deleted successfully" });
+    } else {
+      res.status(404).json({ error: "Task not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const deleteAllTasks = async (req, res) => {
+  try {
+    await Task.destroy({ where: {} });
+    res.status(200).json({ message: "All tasks deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
--- api/src/models/task.js
+++ api/src/models/task.js
@@ -1,43 +1,101 @@
 import { DataTypes } from "sequelize";
 import { db } from "../config/db.js";
+import { Comment } from "./comment.js";
 
-export const Task = db.define("task", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *   Task:
+ *     type: object
+ *     properties:
+ *       id:
+ *         type: integer
+ *         description: The task ID.
+ *       title:
+ *         type: string
+ *         description: The title of the task.
+ *       description:
+ *         type: string
+ *         description: The description of the task.
+ *       priority:
+ *         type: integer
+ *         description: The priority of the task.
+ *       image:
+ *         type: string
+ *         description: Base64 of the image of the task.
+ *       user_id:
+ *         type: integer
+ *         description: The user ID.
+ *       status:
+ *         type: boolean
+ *         description: The status of the task.
+ */
+
+export const Task = db.define(
+  "task",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [5, 50],
+        notEmpty: true,
+      },
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [5, 100],
+        notEmpty: true,
+      },
+    },
+    priority: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isInt: true,
+        min: 1,
+        max: 3,
+      },
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      foreignKey: true,
+    },
+    statusTask: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [["pending", "in progress", "completed"]],
+      },
+    },
+    status: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
   },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  priority: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  image: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    foreignKey: true,
-  },
-  status: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-  },
-  createdAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-  updatedAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
+  {
+    timestamps: true,
+  }
+);
+
+Task.hasMany(Comment, {
+  foreignKey: "task_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Task, {
+  foreignKey: "task_id",
 });
