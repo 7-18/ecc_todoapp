88e4fc95328cc80bca96d2d01aa9971255ad0a2a
Kevin Brian Briceño <kevinbrianbr@gmail.com>
2024-08-29 01:47:57 -0500

    CORS policy backend deploy

--- api/src/app.js
+++ api/src/app.js
@@ -7,25 +7,14 @@ import viewsRouter from "./routes/views.js";
 
 const app = express();
 
-const allowedOrigins = [
-  "https://main.d28gtcjq0f5p4y.amplifyapp.com",
-  "http://localhost:5173",
-  "http://ec2-54-92-235-228.compute-1.amazonaws.com",
-];
+const corsOptions = {
+  origin: "https://main.d28gtcjq0f5p4y.amplifyapp.com",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: "GET,POST,PUT,PATCH,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-  })
-);
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
